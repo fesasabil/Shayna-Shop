2ac9f297f61cccea993f9e5264ef273ace344188
fesasabil <kijee212@gmail.com>
2020-04-07 21:24:00 +0700

    feat:view and router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import ShoppingCart from '../views/ShoppingCart.vue'
+import Success from '../views/Success.vue'
 
 Vue.use(VueRouter)
 
@@ -11,6 +13,16 @@ const routes = [
     component: Home
   },
   {
+    path: '/cart',
+    name: 'cart',
+    component: ShoppingCart
+  },
+  {
+    path: '/success',
+    name: 'success',
+    component: Success
+  },
+  {
     path: '/about',
     name: 'About',
     // route level code-splitting
@@ -19,7 +31,8 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
-    path: '/product',
+    path: '/product/:id',
+    props: true,
     name: 'Product',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
